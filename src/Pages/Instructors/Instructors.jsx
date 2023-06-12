1020dc3ceecc85631561578c78af5b1c25d7fb21
Mohidul Hasan <mahi110145@gmail.com>
2023-06-12 14:33:45 +0600

    Home page full responsive for mobile and tab

--- src/Pages/Instructors/Instructors.jsx
+++ src/Pages/Instructors/Instructors.jsx
@@ -21,7 +21,7 @@ const Instructors = () => {
     <div>
       <div className="p-4 border gap-6">
         {allInstructor && allInstructor.length > 0 ? (
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-3 justify-items-center">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 justify-items-center">
             {allInstructor.map((allInstructor, index) => (
               <AllInstructor key={index} allInstructor={allInstructor} />
             ))}
